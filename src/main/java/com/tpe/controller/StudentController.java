4d6fb0adfd46cd1548c362a6dc428ed83ef207cf
Ali KOÇAN <alikcn35@gmail.com>
2023-01-18 15:09:07 +0300

    1182023309

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.tpe.controller;
 
 import com.tpe.domain.Student;
+import com.tpe.dto.StudentDTO;
+import com.tpe.repository.StudentRepository;
 import com.tpe.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +24,8 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private StudentRepository studentRepository;
 
     // !!! get all students - butun ogrenciler gelsin
     @GetMapping
@@ -41,11 +49,52 @@ public class StudentController {
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
-    // id ile ogrenci getirelim
+    // id ile ogrenci getirelim @RequestParam ile
     @GetMapping("/query") // http://localhost:8080/students/query?id=1
     public ResponseEntity<Student> getStudent(@RequestParam("id") Long id){
         Student student= studentService.findStudent(id);
         return ResponseEntity.ok(student);
+    }
+
+    // id ile ogrenci getirelim @PathVariable ile
+    @GetMapping("{id}") // http://localhost:8080/students/1
+    public ResponseEntity<Student> getStudentWithPath(@PathVariable("id") Long id){
+        Student student= studentService.findStudent(id);
+        return ResponseEntity.ok(student);
+    }
+
+    // !!! delete
+    @DeleteMapping("/{id}") // http://localhost:8080/students/1
+    public ResponseEntity<Map<String, String>> deleteStudent(@PathVariable("id") Long id){
+        studentService.deleteStudent(id);
+        Map<String, String> map=new HashMap<>();
+        map.put("message", "Student is deleted successfuly");
+        map.put("status","true");
+        return new ResponseEntity<>(map, HttpStatus.OK); // return ResponseEntity.ok(map); // ayni sey
+    }
+
+    // !!! update
+    @PutMapping("{id}") // http://localhost:8080/students/1
+    public ResponseEntity<Map<String, String>> updateStudent(
+            @PathVariable("id") Long id,@Valid @RequestBody StudentDTO studentDTO){
+        studentService.updateStudent(id,studentDTO);
+        Map<String, String> map=new HashMap<>();
+        map.put("message", "Student is updated successfuly");
+        map.put("status","true");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    // pageable
+    @GetMapping("/page")
+    public ResponseEntity<Page<Student>> getAllWithPage(
+            @RequestParam("page") int page, // hangi page gonderilecek
+            @RequestParam("size") int size, // page basi kac student olacak
+            @RequestParam("sort") String prop, // siralama hangi fielda gore yapilacak
+            @RequestParam("direction")Sort.Direction direction // dogal sirali mi olsun
+            ){
+        Pageable pageable= PageRequest.of(page,size,Sort.by(direction,prop));
+        Page<Student> studentPage=studentService.getAllWithPage(pageable);
+        return ResponseEntity.ok(studentPage);
     }
 
 }
--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -1,10 +1,13 @@
 package com.tpe.service;
 
 import com.tpe.domain.Student;
+import com.tpe.dto.StudentDTO;
 import com.tpe.exception.ConflictException;
 import com.tpe.exception.ResourceNotFoundException;
 import com.tpe.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,5 +32,30 @@ public class StudentService {
     public Student findStudent(Long id) {
         return studentRepository.findById(id).orElseThrow(
                 ()-> new ResourceNotFoundException("Student not found with id : " +id));
+    }
+
+    public void deleteStudent(Long id) {
+        Student student=findStudent(id);
+        studentRepository.delete(student);
+    }
+
+    public void updateStudent(Long id, StudentDTO studentDTO) {
+        boolean existsEmail=studentRepository.existsByEmail(studentDTO.getEmail())    ;
+        Student student=findStudent(id);
+        if (existsEmail && ! studentDTO.getEmail().equals(student.getEmail())){
+            throw new ConflictException("Email is already exist");
+        }
+
+        student.setName(studentDTO.getFirstName());
+        student.setLastName(studentDTO.getLastName());
+        student.setGrade(studentDTO.getGrade());
+        student.setEmail(studentDTO.getEmail());
+        student.setPhoneNumber(studentDTO.getPhoneNumber());
+        studentRepository.save(student);
+
+    }
+
+    public Page<Student> getAllWithPage(Pageable pageable) {
+        return studentRepository.findAll(pageable);
     }
 }
