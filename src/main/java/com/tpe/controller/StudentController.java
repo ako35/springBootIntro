68b49e7f4b28e9b8353f052976f9db291b0266ed
Ali KOÇAN <alikcn35@gmail.com>
2023-01-19 16:25:42 +0300

    1192023425

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.tpe.domain.Student;
 import com.tpe.dto.StudentDTO;
 import com.tpe.repository.StudentRepository;
 import com.tpe.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +24,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/students") // http://localhost:8080/students
 public class StudentController {
+
+    Logger logger=LoggerFactory.getLogger(StudentController.class);
 
     @Autowired
     private StudentService studentService;
@@ -116,6 +121,12 @@ public class StudentController {
     public ResponseEntity<StudentDTO> getStudentDTO(@RequestParam("id") Long id){
         StudentDTO studentDTO=studentService.findStudentDTOById(id);
         return ResponseEntity.ok(studentDTO);
+    }
+
+    @GetMapping("/welcome") // http://localhost:8080/students/welcome
+    public String welcome(HttpServletRequest request){ // HttpServletRequest ile request e ulastim
+        logger.warn("-----------------Welcome {}", request.getServletPath());
+        return "Student Controller a Hos Geldiniz...";
     }
 
 }
